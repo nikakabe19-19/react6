623438fba699d633834d912f07e0ed88c39235a4
nikakabe19-19 <nikakabe19@gmail.com>
2026-02-27 01:28:58 +0400

    fix:fixed

--- src/components/__organisms/MainDiv/mainDiv.jsx
+++ src/components/__organisms/MainDiv/mainDiv.jsx
@@ -1,14 +1,16 @@
 import Smalldiv from "../../__atoms/Smalldiv/Smalldiv";
-
+import image from "../../..//assets/money.png";
 import SecondDiv from "../../__moleculs/SecondDiv/SecondDiv";
 import { useState } from "react";
 
 function Main_Div() {
   const [number, setNumber] = useState();
+  const [display, setDisplay] = useState();
+  const [last, setLast] = useState("none");
 
   return (
     <>
-      <div className="main_div">
+      <form action="" className="main_div" style={{ display: display }}>
         <SecondDiv />
 
         <div className="oval_div1">
@@ -49,7 +51,31 @@ function Main_Div() {
           />
         </div>
         <div className="last_box">
-          <button className="btn">SUBMIT</button>
+          <button
+            className="btn"
+            onClick={(e) => {
+              e.preventDefault();
+              setDisplay("none");
+              setLast("flex");
+            }}
+          >
+            submit
+          </button>
+        </div>
+      </form>
+      <div className="lastbox3" style={{ display: last }}>
+        <img className="image_box" src={image} />
+        <div className="selsect">
+          <h1 className="orange">You selected 4 out of 5</h1>
+
+          <h1 className="thaks">Thank you!</h1>
+        </div>
+        <div className="bxbx">
+          <h1 className="support">
+            We appreciate you taking the time to give a rating. <br /> If you
+            ever need more support, don’t hesitate to <br />
+            <span> get in touch!</span>
+          </h1>
         </div>
       </div>
     </>
